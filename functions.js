497ec7e2f3cee0d45499e6d858259c6bce98d06c
Eduardo Breda <edu12561@hotmail.com>
2021-03-18 13:38:13 -0300

    study while vs loop and functions

---
+++ functions.js
@@ -0,0 +1,43 @@
+/* functions
+1- create a function
+2- call the function  using fun();
+*/
+
+//create
+function fun (){
+    console.log('this is a function');
+}
+
+//call function
+fun(); 
+
+/* lets create a function that take in a name 
+and says hello followed by your name 
+
+example:
+name: ''Eduardo''
+returns: ''hello Eduardo''
+*/
+
+
+function greeting(yourName) {
+  
+    var result = 'Hello' + ' ' + yourName;  //the ' ' after + is used to create a space between hello and name (string concatenation)
+    console.log(result);
+}
+
+var name = prompt('what is your name?');
+greeting(name);
+
+
+
+
+
+// how do arguments work in functions?
+// How do we add 2 numbers together in a function?
+
+function sumNumbers(num1, num2){
+    var  result = num1 + num2;
+    console.log(result);
+}
+sumNumbers(10, 10); // use any numbers inside those parenthesis to make a sum +, can also use word, for example sumNumbers('hello','eduardo') = hello eduardo
---
+++ whilevsloop.js
@@ -0,0 +1,15 @@
+ /* while loops
+
+ var num = 0;
+
+ while (num < 100) {
+     num += 1;
+     console.log(num);
+ }
+
+*/
+
+// for loop
+for (let num = 0; num <100; num++) { // is the same as while (num) but is a better way to write and read the code
+    console.log(num);
+}
