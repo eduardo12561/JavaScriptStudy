3e9f35f19eb4bc11fb5936c7e666d7ecbf01474f
Eduardo Breda <edu12561@hotmail.com>
2021-03-22 13:08:03 -0300

    study strings, lenght,indexof,slice,replace,touppercase,lowercase,charat,slpit

--- strings/strings.js
+++ strings/strings.js
@@ -1,3 +1,16 @@
 // Strings in JavaScript (common methods)
-let fruit = 'banana';
+let fruit = 'banana, apple, orange,blackberry';
 let moreFruits = 'banana\napple';
+
+console.log(fruit.length);
+console.log(fruit.indexOf('a')); 
+console.log(fruit.slice(2, 7));
+console.log(fruit.replace('ban', '123')); 
+console.log(fruit.toUpperCase()); 
+console.log(fruit.toLowerCase());
+console.log(fruit.charAt(2));
+console.log(fruit[2]);
+console.log(fruit.split(',')); // split by a comma 
+console.log(fruit.split('')); // split by characters
+
+
