fc67430564aef957c5d43248a5a455c39819e766
Eduardo Breda <edu12561@hotmail.com>
2021-03-22 12:41:35 -0300

    study datatypes: number, string, object, array, undefined and value null

---
+++ javascript/datatypes.js
@@ -0,0 +1,8 @@
+// data types
+let yourAge = 18; //number
+let yourName = 'Eduardo'; //string
+let name = {first: 'Edaurdo', last: 'Breda'}; // oject
+let truth = false; // boolean
+let groceries = ['apple', 'banna', 'oranges']; // array
+let random; // undefined
+let nothing = null; // value null 
